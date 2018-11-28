f98eed5cbc386455d449ff29bb6cac21210d061f
AGS <ahmetgokhansener@gmail.com>
2018-11-28 23:08:30 +0300

    Ana sayfa tasarımı yapıldı

--- app/src/main/java/com/ags/keopsandroidapp/AddPhotoActivity.java
+++ app/src/main/java/com/ags/keopsandroidapp/AddPhotoActivity.java
@@ -1,27 +1,17 @@
 package com.ags.keopsandroidapp;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -30,14 +20,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class Main2Activity extends AppCompatActivity {
+public class AddPhotoActivity extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_addphoto);
 
 
         //Fotografın galeriden istendigi kisim
@@ -130,7 +120,7 @@ public class Main2Activity extends AppCompatActivity {
 
             dataToSend = "image=" + params[0];
             communicator(completeServerAddress, dataToSend);
-            
+
             return null;
         }
 
--- app/src/main/java/com/ags/keopsandroidapp/MainActivity.java
+++ app/src/main/java/com/ags/keopsandroidapp/MainActivity.java
@@ -1,14 +1,12 @@
 package com.ags.keopsandroidapp;
 
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,16 +14,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater .inflate(R.menu.add_albums, menu);
-        return  super.onCreateOptionsMenu(menu);
+        menuInflater.inflate(R.menu.add_albums, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         //Menu item'nin secilmesi kontrolu
-        if(item.getItemId() == R.id.add_albums){
+        if (item.getItemId() == R.id.add_albums) {
             //MainActivit2'nin calismasi saglaniyor
-            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            Intent intent = new Intent(getApplicationContext(), AddPhotoActivity.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
@@ -34,12 +32,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_homepage);
 
         Intent intent = getIntent();
         String userID = intent.getStringExtra("userId");
         System.out.println("In the MainActivity Page, the authenticated user_id: " + userID );
     }
-    
+
+    public void showAlbum(View view) {
+
+    }
+
+    public void sharedWithMe(View view) {
+
+    }
+
 
 }
